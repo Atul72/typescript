c94fe0c8e7ef09922d5ff7c9bd1d173f92e88318
Atul72 <rai703537@gmail.com>
2023-11-09 18:43:43 +0530

    add first lesson

---
+++ Lesson1/build/js/main.js
@@ -0,0 +1,22 @@
+"use strict";
+// let username = "atul";
+// console.log(username);
+// let a: number = 12;
+// let b: string = "6";
+// let c: number = 2;
+// console.log(a);
+let myName = "atul";
+let meaningOfLife;
+let isLoading;
+let album; // union type
+myName = "tilak";
+meaningOfLife = 89;
+isLoading = true;
+album = 89;
+album = "yhh";
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
